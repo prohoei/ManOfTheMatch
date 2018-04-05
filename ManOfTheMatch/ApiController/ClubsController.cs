0821023ebb77aa961d7b941ebe7be09e1755371c
Alexander Høi <heyman2525@hotmail.com>
2018-04-05 14:24:18 +0200

    Umbrac Api Project Created
    Custom Api build with Umbraco CMS on ASP.NET

---
+++ ManOfTheMatch/ApiController/ClubsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace ManOfTheMatch.ApiController
+{
+    public class ClubsController : UmbracoAuthorizedApiController
+    {
+        // Custom api get
+        public IEnumerable<string> GetAllProducts()
+        {
+            return new[] { "Table", "Chair", "Desk", "Computer", "Beer fridge" };
+        }
+            
+        // GET: api/Clubs
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Clubs/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Clubs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Clubs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Clubs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
